4ad8af91622a63db9e11a2d6b59e245f03ecc1dc
pberd21 <berdnikov0500@gmail.com>
2024-08-23 16:43:37 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,17 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(a=34)
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+value_list = [21, 'Привет', True]
+print_params(*value_list)
+
+value_dict = {'a': 3, 'b': "Словарик", 'c': [6, 3, 9]}
+print_params(**value_dict)
+
+value_list2 = [54.32, 'Cтрока']
+print_params(*value_list2, 42)
